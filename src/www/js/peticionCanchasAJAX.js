4684c3002b58af959f2856304319bb15461d468f
Víctor Hernández <alu0100697032@ull.edu.es>
2015-11-25 23:09:37 +0000

    Código optimizado

--- src/www/js/peticionCanchasAJAX.js
+++ src/www/js/peticionCanchasAJAX.js
@@ -5,6 +5,8 @@ $(document).ready(function() {
       var data = result;
       var subContent = "";
       var title = "";
+      var subContent2 = "";
+      var title2 = "";
       var titleAnterior = "";
 
       for(var i = 0; i < data.length; i++) {
@@ -21,15 +23,14 @@ $(document).ready(function() {
           '</h1>'+  subContent +'</div>';
           subContent = "";
           $div.append(content);
+
+          title2 = data[i].First_NOM_;
+          subContent2 += '<p>'+  data[i].First_POB1 +'</p><p>'+ data[i].First_DIR_ +'</p>';
+          var content2 = '<div data-role="collapsible" data-collapsed="false"><h1>' + title2 +
+          '</h1>'+  subContent2 +'</div>';
+          subContent2 = "";
+          $div2.append(content2);
         }
-      }
-      for(var i = 0; i < data.length; i++) {
-        title = data[i].First_NOM_;
-        subContent += '<p>'+  data[i].First_POB1 +'</p><p>'+ data[i].First_DIR_ +'</p>';
-        var content = '<div data-role="collapsible" data-collapsed="false"><h1>' + title +
-        '</h1>'+  subContent +'</div>';
-        subContent = "";
-        $div2.append(content);
       }
       $('#list_view_canchas').collapsibleset('refresh');
       $('#list_view_poblacion').collapsibleset('refresh');
